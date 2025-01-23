74e0c9de567b39515365fb6b631109db09928bc9
raxvan <razvan.om@gmail.com>
2025-01-23 12:22:05 +0000

    * small install fix

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup, find_packages
 
+def ldescription():
+    try:
+        with open('README.md') as f:
+            return f.read()
+    except:
+        return ""
+
 setup(
     name = "workspace-package-manager",
     version = "0.0.1",
     description = "This is a package manager designed for developers.",
-    long_description = open('README.md').read(),
+    long_description = ldescription(),
     long_description_content_type = 'text/markdown',
     packages = find_packages(),
     install_requires = [
